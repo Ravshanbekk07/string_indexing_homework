882a3b85ab1c5dfe23a85e3003a98ab524510640
Ravshanbekk07 <ramazonof07@gmail.com>
2023-05-05 10:27:58 +0500

    good

--- Index10.py
+++ Index10.py
@@ -6,4 +6,14 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    a = int(s)
+    x1  = a%10
+    x2 = a%100//10
+    x3 = a%1000//100
+    x4 = a//1000%10
+    x5 = a//1000//10
+    return x5 +x1+x2+x3+x4
+
+
+v = main('12332')
+print(v)
